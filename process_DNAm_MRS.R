c0a1e460c656ec60abcf082a4fc3ca4e202d187d
Ella Davyson <ellad@MacBook-Air-332.local>
2023-11-21 13:20:35 +0000

    Testing scripts- adding plots to R

--- process_DNAm_MRS.R
+++ process_DNAm_MRS.R
@@ -4,11 +4,15 @@
 
 ###############################################################################
 
+.libPaths('/exports/igmm/eddie/GenScotDepression/users/edavyson/R/x86_64-pc-linux-gnu-library/4.1')
 
 library(data.table)
 library(dplyr)
 library(optparse)
 library(readr)
+library(tools)
+library(ggplot2)
+library(tidyr)
 
 parse <- OptionParser()
 
@@ -16,7 +20,7 @@ parse <- OptionParser()
 option_list <- list(
   make_option('--DNAm', type='character', help="The filepath for DNAm file", action='store'),
   make_option('--probes', type = 'character', help= "The filepath for the list of probes for the MRS", action = 'store'),
-  make_option('--id_columns', type = 'character', nargs=2, default=c("FID", "IID"), help = "Column names of identifier column(s)", action = 'store'),
+  make_option('--id_column', type = 'character', default="IID", help = "Column names of identifier column", action = 'store'),
   make_option('--out', type = 'character', help = 'The filepath for output', action = 'store')
 )
 
@@ -24,11 +28,17 @@ args = commandArgs(trailingOnly=TRUE)
 opt <- parse_args(OptionParser(option_list=option_list), args=args)
 
 # setting up arguments from the options 
-
+print('Setting up the options')
 DNAm_filepath=opt$DNAm # DNAm file
 probes_filepath=opt$probes # Probe list 
-id_cols <- opt$id_columns # Vector of identifier columns 
+id_col <- opt$id_column # Vector of identifier columns 
 outfile <- opt$out
+
+sink(paste0(file_path_sans_ext(outfile), ".log"))
+print(paste0('DNAm file from : ', DNAm_filepath))
+print(paste0('List of probes from : ', probes_filepath))
+print(paste0('ID column : ', id_col))
+print(paste0('Output to be saved : ', outfile))
 
 ###############################################################################
 
@@ -45,27 +55,58 @@ if (endsWith(DNAm_filepath, ".rds")){
 }
 
 probes <- readr::read_lines(probes_filepath)
+print('Read in files')
+print(paste0('Number of probes in DNAm file: ',ncol(DNAm)))
 
 ###############################################################################
 
 # Filter the DNAm to just the CpGs used in the MRS (and identifiers)
 
 ###############################################################################
+print('Filter to just the MRS CpGs')
+DNAm_MRS <- DNAm %>% select(c(all_of(id_col), intersect(names(DNAm), probes)))
 
-DNAm_MRS <- DNAm %>% select(c(id_cols, intersect(names(DNAm), probes)))
+print(paste0('Filtered to ', ncol(DNAm_MRS)-1, ' CpGs')) # not counting the ID column
+rm(DNAm) # remove large methylation object 
 
 ###############################################################################
 
 # Scale the DNAm levels 
 
 ###############################################################################
+print('Scaling the CpG columns')
 
-DNAm_MRS_std <- DNAm_MRS %>% mutate(across(-c(FID, IID), scale))
+DNAm_MRS_std <- DNAm_MRS %>% mutate(across(-c(all_of(id_col)), scale))
+
+# pick a random CpG to plot the distribution?
+
+cpgs <- sample(setdiff(names(DNAm_MRS_std), id_col), 3)
+DNAm_both <- rbind(
+DNAm_MRS %>% 
+  select(c(all_of(id_col), all_of(cpgs))) %>%
+  pivot_longer(cols = -c(all_of(id_col)), 
+               names_to = "CpG", 
+               values_to = "Mval") %>% 
+  as.data.frame() %>% 
+  mutate(Values = 'Unstandardised'),
+DNAm_MRS_std %>% 
+  select(c(all_of(id_col), all_of(cpgs))) %>%
+  pivot_longer(cols = -c(all_of(id_col)), 
+               names_to = "CpG", 
+               values_to = "Mval") %>%
+  mutate(Values = 'Standardised')
+)
+
+DNAm_dists <- ggplot(DNAm_both, aes(x = Mval, fill = CpG)) + geom_histogram() + facet_grid(Values~CpG) + ggtitle('Random sample of probes - standardisation')
+ggsave(filename=paste0(file_path_sans_ext(outfile), ".png"),DNAm_dists, width = 8, height = 6, device='png', dpi=300)
+
 
 ###############################################################################
 
 # Write out the filtered and standardised DNAm data 
 
 ###############################################################################
+print(paste0('Writing the processed DNAm file to ', outfile))
 
-write.table(DNA_MRS_std, outfile, row.names = F, quote = F)
+write.table(DNAm_MRS_std, outfile, row.names = F, quote = F)
+sink()
--- process_DNAm_OSCA.sh
+++ process_DNAm_OSCA.sh
@@ -1,3 +1,4 @@
 module load igmm/apps/osca/0.46 # or equivilent 
 
---osca --befile DNAm_filename --extract-probe selfrep_lasso_probe_lst.txt --std-probe --make-efile --out outfile_name
+osca --befile DNAm_filename --extract-probe selfrep_lasso_probe_lst.txt --std-probe --make-efile --out outfile_name
+

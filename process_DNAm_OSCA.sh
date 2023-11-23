aa374e3ed0d0b3faf506385f416a949d1f0dcc09
Ella Davyson <ellad@macbook-air-332.lan>
2023-11-23 10:43:46 +0000

    adding cohort argument

--- process_DNAm_OSCA.sh
+++ process_DNAm_OSCA.sh
@@ -1,4 +1,5 @@
 module load igmm/apps/osca/0.46 # or equivilent 
 
-osca --befile DNAm_filename --extract-probe selfrep_lasso_probe_lst.txt --std-probe --make-efile --out outfile_name
+cohort="" #Input the cohort name , e.g "GS"
+osca --befile DNAm_filename --extract-probe selfrep_lasso_probe_lst.txt --std-probe --make-efile --out ${cohort}_DNAm_preproc
 

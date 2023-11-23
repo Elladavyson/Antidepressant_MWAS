a8a7c0af3fe1a5c55c3b21dae166432a87513739
Ella Davyson <ellad@MacBook-Air-332.local>
2023-11-23 16:43:01 +0000

    Updating file structures- files now in distinct folders

--- process_DNAm_OSCA.sh
+++
@@ -1,5 +0,0 @@
-module load igmm/apps/osca/0.46 # or equivilent 
-
-cohort="" #Input the cohort name , e.g "GS"
-osca --befile DNAm_filename --extract-probe selfrep_lasso_probe_lst.txt --std-probe --make-efile --out ${cohort}_DNAm_preproc
-

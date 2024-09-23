db4d83574f8b50b997af84e0548169af510783cb
Ella Davyson <elladavyson@gmail.com>
2024-09-23 13:11:00 -0400

    Updating MWAS scripts for standardised male and female DNAm data

--- MWAS_sex.sh
+++ MWAS_sex.sh
@@ -6,8 +6,8 @@
 #$ -l rl9=false
 #$ -cwd
 #$ -pe sharedmem 8
-#$ -e /exports/igmm/eddie/GenScotDepression/users/edavyson/antidep_project/revisions/output/MWAS_sex_segregated/MOA_output/MOA_logs
-#$ -o /exports/igmm/eddie/GenScotDepression/users/edavyson/antidep_project/revisions/output/MWAS_sex_segregated/MOA_output/MOA_logs
+#$ -e /exports/igmm/eddie/GenScotDepression/users/edavyson/antidep_project/revisions/output/MWAS_sex_segregated/MOA_output/sex_standardised/MOA_logs
+#$ -o /exports/igmm/eddie/GenScotDepression/users/edavyson/antidep_project/revisions/output/MWAS_sex_segregated/MOA_output/sex_standardised/MOA_logs
 #$ -M s2112198@ed.ac.uk
 #$ -m baes
 
@@ -21,15 +21,15 @@ SCRATCH='/exports/eddie/scratch/s2112198'
 pheno=$1
 sex=$2
 
-OUTPUTDIR="/exports/igmm/eddie/GenScotDepression/users/edavyson/antidep_project/revisions/output/MWAS_sex_segregated/MOA_output/"
+OUTPUTDIR="/exports/igmm/eddie/GenScotDepression/users/edavyson/antidep_project/revisions/output/MWAS_sex_segregated/MOA_output/sex_standardised"
 RESIDDIR="/exports/igmm/eddie/GenScotDepression/users/edavyson/antidep_project/antidep_phenotypes/phenotypes/05_23_phenos/resid_phenos"
 
 pheno_filename="residualised_"${pheno}"_"${sex}"nocolnames.pheno"
 
 echo "Phenotype file: ${pheno_filename}"
 
-output_filename="${OUTPUTDIR}/"${pheno}"_GRMunadj_ORM_residph_"${sex}"_16_09"
+output_filename="${OUTPUTDIR}/"${pheno}"_GRMunadj_sexstd_ORM_residph_"${sex}"_23_09"
 
 echo "Output to be saved to file: ${output_filename}"
 
-osca --moa --befile $SCRATCH/mvals_GRM_uncorrected_29_06_OSCA_standard --pheno $SCRATCH/residualised_${pheno}_${sex}_nocolnames.pheno --qcovar $SCRATCH/qcovs_11_07_GRMFID_nocolnames --covar $SCRATCH/covs_11_07_GRMFID_${sex}_nocolnames --thread-num 8 --out ${output_filename}
+osca --moa --befile $SCRATCH/mvals_GRMuncor_${sex}_standardised_23_09 --pheno $SCRATCH/residualised_${pheno}_${sex}_nocolnames.pheno --qcovar $SCRATCH/qcovs_11_07_GRMFID_nocolnames --covar $SCRATCH/covs_11_07_GRMFID_${sex}_nocolnames --thread-num 8 --out ${output_filename}
